ecdfd3ed675f8560ca43b3652bf677e5ee914db6
Jim Groome <jim.groome@marketingdelivery.com>
2019-06-24 15:34:00 +0100

    Added social media links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import ReactGA from 'react-ga';
+import React from "react";
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+import ReactGA from "react-ga";
 
 function App() {
-
-  ReactGA.initialize('UA-9828579-19');
+  ReactGA.initialize("UA-9828579-19");
   ReactGA.pageview(window.location.pathname);
 
-  const colours = [
-    'blue',
-    'green',
-    'yellow',
-    'pink'
-  ];
+  const colours = ["blue", "green", "yellow", "pink"];
 
   const colour = colours[Math.floor(Math.random() * colours.length)];
-  const containerClass = 'page-container ' + colour;
+  const containerClass = "page-container " + colour;
 
   return (
-
     <div className={containerClass}>
       <Container fluid={true}>
         <Row>
@@ -28,7 +21,26 @@ function App() {
         </Row>
         <Row>
           <Col>
-            <p>Web developer. Music fan, reader, excessively tall, hot drinks enthusiast.</p>
+            <p>
+              Web developer. Music fan, reader, excessively tall, hot drinks
+              enthusiast.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Nav>
+              <NavItem>
+                <NavLink href='https://github.com/jimgroome' target='_blank'>
+                  <i className='fab fa-github' /> Github
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='mailto:hello@jimgroo.me' target='_blank'>
+                  <i className='fas fa-at' /> Email
+                </NavLink>
+              </NavItem>
+            </Nav>
           </Col>
         </Row>
       </Container>
