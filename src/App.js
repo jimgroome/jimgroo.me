fae673d8d5d045ca259ab5344c51da0e80c36ca8
Jim Groome <jim.groome@marketingdelivery.com>
2019-06-26 14:48:15 +0100

    PWA tweaks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactGA from 'react-ga';
-import SiteContent from './components/SiteContent';
+import React from "react";
+import ReactGA from "react-ga";
+import SiteContent from "./components/SiteContent";
 
 function App() {
-  ReactGA.initialize('UA-9828579-19');
+  ReactGA.initialize("UA-9828579-19");
   ReactGA.pageview(window.location.pathname);
 
-  const colours = ['blue', 'green', 'yellow', 'pink'];
+  const colours = ["blue", "green", "yellow", "pink"];
 
   const colour = colours[Math.floor(Math.random() * colours.length)];
-  const containerClass = 'page-container ' + colour;
+  const containerClass = "page-container " + colour;
 
   return (
     <div className={containerClass}>
-      {/* <Navigation /> */}
       <SiteContent />
     </div>
   );
