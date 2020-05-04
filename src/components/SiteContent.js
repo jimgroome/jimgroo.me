f02f111922ee9effe0555bc449d2248145d935dc
Jim Groome <jim.groome@take1.tv>
2020-05-04 14:19:31 +0100

    New copy and simplified style

--- src/components/SiteContent.js
+++ src/components/SiteContent.js
@@ -1,40 +1,21 @@
-import React, { Fragment } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React from "react";
 
 const SiteContent = () => {
   return (
-    <Fragment>
-      <Container>
-        <Row>
-          <Col className="site-content-container">
-            <div className="site-content">
-              <h1 className="mb-4">Jim Groome</h1>
-              <p>
-                Web developer in Kent. Music fan, reader, excessively tall, hot
-                drinks enthusiast.
-              </p>
-              <p className="mb-0">
-                <a
-                  href="https://github.com/jimgroome"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Github
-                </a>{' '}
-                ::{' '}
-                <a
-                  href="mailto:hello@jimgroo.me"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Email
-                </a>
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
+    <div className="site-content">
+      <h1 className="mb-4">Jim Groome</h1>
+      <p>Full-stack web developer in Kent.</p>
+      <p>Loves HTML, CSS, JS, Python, AWS, JAMstack, and hot drinks.</p>
+      <p className="mb-0">
+        <a href="https://github.com/jimgroome" rel="noopener noreferrer" target="_blank">
+          Github
+        </a>{" "}
+        ::{" "}
+        <a href="mailto:hello@jimgroo.me" rel="noopener noreferrer" target="_blank">
+          Email
+        </a>
+      </p>
+    </div>
   );
 };
 
