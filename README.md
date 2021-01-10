cf95d25863bb9f89addd63220d9aa94ddbe6257c
Jim Groome <james@jgroome.com>
2021-01-10 21:11:31 +0000

    Removed some content and changed where the page is hosted

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # jimgroo.me
 
-A very basic landing page built using React and hosted here on Github pages.
+A very basic landing page built using React.
 
 ## Notes to self
 
 - Clone the repository
 - `npm i` to install
 - `npm run start` for development
-- `npm run deploy` builds the site and pushes to the _gh-pages_ branch, updating the live site
+- `npm run deploy` builds the site and uploads to S3
--- src/components/SiteContent.js
+++ src/components/SiteContent.js
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import React from "react";
 
-const SiteContent = ({ colour }) => {
-  const [workModalOpen, setWorkModalOpen] = useState(false);
-
-  const toggleWorkModal = (e) => {
-    e.preventDefault();
-    setWorkModalOpen(!workModalOpen);
-  };
-
+const SiteContent = () => {
   return (
     <div className="site-content">
       <h1 className="mb-4">Jim Groome</h1>
       <p>Full-stack web developer in Kent.</p>
       <p>Loves HTML, CSS, JS, Python, AWS, JAMstack, and hot drinks.</p>
       <p className="mb-0">
-        <a href="#!" onClick={(e) => toggleWorkModal(e)}>
-          Work
-        </a>{" "}
-        ::{" "}
         <a href="https://github.com/jimgroome" rel="noopener noreferrer" target="_blank">
           Github
         </a>{" "}
@@ -27,42 +15,6 @@ const SiteContent = ({ colour }) => {
           Email
         </a>
       </p>
-      <Modal isOpen={workModalOpen} toggle={toggleWorkModal}>
-        <ModalHeader toggle={toggleWorkModal}>Selected recent work</ModalHeader>
-        <ModalBody>
-          <p>
-            Most of my work is for my employer, who do not make their products public. However, here are some freelance
-            projects I've worked on recently.
-          </p>
-          <ul>
-            <li>
-              <a href="https://nowplaying.jimgroo.me" target="_blank" rel="noopener noreferrer">
-                Now playing on BBC 6 Music
-              </a>
-            </li>
-            <li>
-              <a href="https://www.wesanitizeservices.com/" target="_blank" rel="noopener noreferrer">
-                We Sanitize Services
-              </a>
-            </li>
-            <li>
-              <a href="https://fierce-content.com/" target="_blank" rel="noopener noreferrer">
-                Fierce Content
-              </a>
-            </li>
-            <li>
-              <a href="https://weddingphotos.jimgroo.me/" target="_blank" rel="noopener noreferrer">
-                Wedding photos
-              </a>
-            </li>
-          </ul>
-        </ModalBody>
-        <ModalFooter>
-          <Button onClick={toggleWorkModal} color="secondary">
-            Close
-          </Button>
-        </ModalFooter>
-      </Modal>
     </div>
   );
 };
