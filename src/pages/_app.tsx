15bf7f19049d7ac03574b4fb5bd5bfc3b004e097
Jim Groome <james@jgroome.com>
2023-04-03 21:21:34 +0100

    More font fun

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import type { AppProps } from "next/app";
+import { Bitter } from "next/font/google";
+
+const bitter = Bitter({ weight: "500", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={bitter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
